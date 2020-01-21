6d2b2a1544ac34131845657b04c65825c7a0c5df
Brian Leonardo de Morais <morais.brian360@gmail.com>
2020-01-21 20:08:24 -0300

    Aula 9 concluída

--- PaymentContext/PaymentContext.Tests/Entities/StudentTests.cs
+++ PaymentContext/PaymentContext.Tests/Entities/StudentTests.cs
@@ -1,13 +1,16 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
 
 namespace PaymentContext.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class StudentTests
     {
         [TestMethod]
         public void TestMethod1()
         {
+            var student = new Student("Brian", "Morais", "2010245", "contato@gmail.com");
+            
         }
     }
 }
